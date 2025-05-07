dceec7e9262ef9e81adea7fd0abb8e84a7033bb5
Muthu kumar <muthukumar24081999@gmail.com>
2025-05-08 00:08:35 +0530

    fourth commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const userRouter = require("./routes/userRoute.js");
 const castRouter = require("./routes/castRoute.js");
 const movieRouter = require("./routes/movieRoute.js");
@@ -11,9 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // middlewares
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 
 // api endpoints
 app.use("/user", userRouter);
@@ -22,7 +22,6 @@ app.use("/movie", movieRouter);
 
 mongoose
   .connect(process.env.MONGODB)
-  // .connect(process.env.MONGODB)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
